3bf75e7bc88d7e1cf1accb4a755bbabe5334ceaa
christiano0407 <placebo0704@gmail.com>
2026-05-29 12:06:32 -0600

    [API/FastAPI - OpenAPI] Develop API: v1 & Part: 2

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
   DEBUG: bool = False
   LOG_LEVEL: str = "INFO"
   #TODO === Server === #
-  HOST: str = "0.0.0.0"
+  HOST: str = "0.0.0.0"  # noqa: S104
   PORT: int = 8000
   WORKERS: int = 1
   RELOAD: bool = False
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
   CACHE_TTL_SECONDS: int = 300 # 5 min Default
 
   #* === JWT (JSON Web Tokens) === #
-  JWT_SECRET_KEY: str = "CHANGE_ME_IN_PRODUCTION_use_32+_random_chars"
+  JWT_SECRET_KEY: str = "CHANGE_ME_IN_PRODUCTION_use_32+_random_chars"  # noqa: S105
   JWT_ALGORITHM: str = "HS256"
   JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
   JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
@@ -53,13 +53,52 @@ class Settings(BaseSettings):
   REFRESH_COOKIE_SAMESITE: Literal["strict", "lax", "none"] = "lax"
 
   #! === CORS ( Conexión con Navegadores / Browser ) === #
+  CORS_ORIGIN: list[AnyHttpUrl | str] = ["http://localhost:3000", "http://localhost:5173"]
+  CORS_ALLOW_CREDENTIALS: bool = True
 
   #! === Rate Limiting (Límite de 'Request' / Peticiones) === #
+  RATE_LIMIT_PUBLIC: int = 30  # req/min
+  RATE_LIMIT_AUT: int = 10 # req/min
 
   #* === DB: MinIO / Storage s3 === # 
+  STORAGE_ENDPOINT: str = "localhost:9000"
+  STORAGE_ACCESS_KEY: str = "minioadmin"
+  STORAGE_SECRET_KEY: str = "minioadmin"  # noqa: S105
+  STORAGE_BUCKET: str = "fifa-data"
+  STORAGE_SECURE: bool = False
 
   #! === Celery / ARQ (ETL async) === #
+  BROKER_URL: str = "redis://localhost:6379/1"
 
   #?? ── Docs ─────────────────────────────────────────────────────────────────
+  @property
+  def docs_enable(self) -> bool:
+      return self.ENVIRONMENT in ("development", "staging")
   
   #?? ── Validators ───────────────────────────────────────────────────────────
+
+  @field_validator("JWT_SECRET_KEY")
+  @classmethod
+  def validate_secret_key(cls, v:str) -> str:
+     if len(v) < 32:  # noqa:PLR2004
+          raise ValueError("jwt_secret_key must be at least 32 characters (Debe tener al menos 32 caracteres.)")  # noqa: E501
+     return v
+  
+  @model_validator(mode="after")
+  def secure_cookie_in_production(self) ->  Settings:
+      if self.ENVIRONMENT == "production" and not self.REFRESH_COOKIE_SECURE:
+        raise ValueError("REFRESH_COOKIE_SECURE must be True in production")
+      return self
+  
+  @property
+  def db_url_str(self) -> str: 
+      return str(self.DATABASE_URL)
+
+  @property
+  def db_redis_url_str(self) -> str:
+      return str(self.REDIS_URL)
+
+
+@lru_cache(maxsize=1)
+def get_setting() -> Settings: 
+    return Settings()
