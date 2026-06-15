bac3ba34345bea4878af0f617bf5368ea4120de7
christiano0407 <placebo0704@gmail.com>
2026-06-15 09:42:14 -0600

    [API REST] CRUD & OpenAPI | Part: 1 - Version: 1.00.04

---
+++ backend/app/api/v1/endpoints/deps.py
@@ -0,0 +1,5 @@
+"""
+  - Este archivo unifica tus generadores de base de datos (get_db), la resolución 
+    de la configuración global, tu cliente de caché y la instanciación limpia 
+    de tu servicio de negocio.
+"""  # noqa: E501
