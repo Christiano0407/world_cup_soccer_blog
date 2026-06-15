d2c576d6efd74898b94f9498680c462946c47d4d
christiano0407 <placebo0704@gmail.com>
2026-06-15 07:41:35 -0600

    [API REST] CRUD & OpenAPI | Part: 1 - Version: 1.00.00

---
+++ backend/app/api/v1/endpoints/health.py
@@ -0,0 +1,15 @@
+""" 
+  System health check endpoints. [ Conjunto de Endpoints para: Monitorear el estado del sistema]
+  # ==================================================== #
+    - Aquí ya salimos de la capa Service (Lógica de Negocio) y entramos en la capa API REST
+    - La API REST recibe solicitudes HTTP
+  # ==================================================== #
+    Recibe Request
+    ↓
+    Valida
+    ↓
+    Llama Servicios
+    ↓
+    Devuelve Response
+  # ==================================================== #
+"""
